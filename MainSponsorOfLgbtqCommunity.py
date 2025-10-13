9665f7ae7dd4af81d0ec13a9fb74e769d3f47800
georgii <mygdshowcases@gmail.com>
2025-10-13 23:49:51 +0200

    added some basic functionality to server.py, also improved client side

--- MainSponsorOfLgbtqCommunity.py
+++ MainSponsorOfLgbtqCommunity.py
@@ -1,184 +1,403 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 import requests
 import json
 import base64
 
-SERVER_URL = "http://127.0.0.1:8080"
 
-'''
-def send_data():
-    data = {
-        "username": entry_username.get(),
-        "name": entry_name.get(),
-        "password": entry_password.get(),
-        "momnum": entry_momnum.get(),
-        "razmer": entry_razmer.get(),
-        "age": entry_age.get()
-    }
+SERVER_URL = "http://127.0.0.1:5000"
+root = tk.Tk()
+USERNAME = None
+PASSWORD = None
 
-    if not all(data.values()):
-        messagebox.showerror("error", "each field must be filled in")
-        return
 
-    try:
-        response = requests.post(SERVER_URL, json=data)
-        if response.status_code == 200:
-            messagebox.showinfo("success", "data sent successfully")
-        else:
-            messagebox.showerror("error", f"The server returned: {response.text}")
-    except requests.exceptions.RequestException as e:
-        messagebox.showerror("error", f"lox: {e}")
-#dfghj
-'''
+def clear_all_inside_frame(frame):
+    # Iterate through every widget inside the frame
+    for widget in frame.winfo_children():
+        widget.destroy()
 
-USERNAME=None
-PASSWORD=None
+def goto(func, args=[]):
+    clear_all_inside_frame(root)
+    func(*args).pack()#(side=tk.NSEW, fill=tk.BOTH, expand=True)
 
-def onStartup():
-    global USERNAME, PASSWORD
+def checkLogin(username, password):
+    data={'username': username, 'password': password}
+    json_data = json.dumps(data)
 
-    savedData = open('clientData.json', 'r').read()
-    data = json.loads(savedData)
-    USERNAME = base64.b64decode(data['username']).decode("utf-8")
-    PASSWORD = base64.b64decode(data['password']).decode("utf-8")
-
-    loginJson = json.dumps({
-        "username": USERNAME,
-        "password": PASSWORD
-    })
-    answer = requests.post(SERVER_URL + '/login', json=loginJson)
+    answer = requests.post(SERVER_URL+'/login', data=json_data, headers={'Content-Type': 'application/json'})
 
     if answer.text == 'True':
-        print("login success")
-        print('Show main page when it is ready')
+        #global USERNAME, PASSWORD
+        #USERNAME = username
+        #PASSWORD = password
+        return True
+    else:
+        return answer
+
+def createNewUser(username, password, pol, nomermami, razmer):
+    data={'username': username, 'password': password, 'pol': pol, 'momnum': nomermami, 'razmer': razmer}
+    json_data = json.dumps(data)
+
+
+    answer = requests.post(SERVER_URL+'/createNewUser2', data=json_data, headers={'Content-Type': 'application/json'})
+
+    if answer.text == 'True':
+        global USERNAME, PASSWORD
+        USERNAME = username
+        PASSWORD = password
+        return True
+    else:
+        return answer
+
+def createNewUserWrap(username, password, pol, nomermami, razmer):
+    created=createNewUser(username, password, pol, nomermami, razmer)
+    #print(created.text)
+    if created==True:
+        goto(main)
+    else:
+        goto(register, args=[created.text])
+
+def checkLoginWrap(username, password):
+    answer=checkLogin(username, password)
+    if answer==True:
+        global USERNAME, PASSWORD
+        USERNAME = username
+        PASSWORD = password
+        saveData(USERNAME, PASSWORD)
+        goto(main)
+    else:
+        goto(login, args=[answer.text])
+
+def onStart():
+    global USERNAME, PASSWORD
+    file=open('clientData.json', 'r')
+    if len(file.read())==0:#if file is empty write none to it
+        #file = open('clientData.json', 'w')
+        #file.write(json.dumps({'username': 'Tm9uZQ==', 'password': 'Tm9uZQ=='}))
+        #file.close()
+        saveData('Tm9uZQ==', 'Tm9uZQ==')
+
+    file = open('clientData.json', 'r')
+    data=json.load(file)
+    USERNAME=base64.b64decode(data['username']).decode()
+    PASSWORD=base64.b64decode(data['password']).decode()
+    #print(USERNAME)
+    #print(PASSWORD)
+
+def saveData(username,password):
+    json.dump({'username': base64.b64encode(username.encode('utf-8')).decode(), 'password': base64.b64encode(password.encode('utf-8')).decode()}, open('clientData.json', 'w'))
+
+def selectFile():
+    filePath=filedialog.askopenfilename(title="Select a file", filetypes=[('photo files', ('*.png', '*.jpg', '*.jpeg'))])
+
+    if filePath:
+        return filePath
+    return None
+
+def selectFileWrap():
+    filePath=selectFile()
+    if filePath is None:
+        return
+    goto(create, args=[filePath])
+
+def makePost(filePath, title):
+    if filePath is None:
+        messagebox.showerror("Error", "Please select a file")
+        return
+    allowedFiletypes=['.png', '.jpg', 'jpeg']
+    if filePath.lower().split('.')[-1] not in allowedFiletypes:
+        messagebox.showerror("Error", "Incorrect file type")
+        return
+    image = open(filePath, 'rb').read()
+
+    data={'username': USERNAME, 'password': PASSWORD, 'title': title, 'file': base64.b64encode(image).decode(), 'fileTitle': filePath.split('/')[-1]}
+    json_data = json.dumps(data)
+    #print(json_data)
+    response = requests.post(SERVER_URL+'/newPost', data=json_data, headers={'Content-Type': 'application/json'})
+    if response.text == 'True':
+        messagebox.showinfo("Success", "Posted successfully")
+        goto(create)
+    else:
+        messagebox.showerror("Error", "Failed to post")
+        goto(create, args=['', response.text])
+
+def logout():
+    saveData('None', 'None')
+    goto(welcome)
+
+def getSubscribes():
+    data={'username': USERNAME, 'password': PASSWORD}
+    answer = requests.get(SERVER_URL+'/getSubscribes', json=data ,headers={'Content-Type': 'application/json'})
+    #print(answer.text)
+    if answer.text != 'invalid credentials':
+        subscribes=json.loads(answer.text)
+        return subscribes
 
     else:
-        USERNAME = None
-        PASSWORD = None
+        return []
 
-def writeToFile(username,password):
-    savedData = open('clientData.json', 'w')
-    usrn=base64.b64encode(username.encode()).decode()
-    pw=base64.b64encode(password.encode()).decode()
-    jsondata = json.dumps({
-        "username": usrn,
-        "password": pw
-    })
-    savedData.write(jsondata)
+def getUsers():
+    answer=requests.get(SERVER_URL+'/getUsers')#, headers={'Content-Type': 'application/json'})
 
-def requestUserRegistration(entry_username, entry_password, entry_momnum, entry_razmer, entry_pol, root):
-    global USERNAME, PASSWORD
-    data={
-        "username": entry_username.get(),
-        "password": entry_password.get(),
-        "momnum": entry_momnum.get(),
-        "razmer": entry_razmer.get(),
-        "pol": entry_pol.get()
-    }
-    datajson = json.dumps(data)
-    print(datajson)
+def requestSearch(prompt):
+    data=json.dumps({'search': prompt})
 
-    dataGood=True
-    for key, value in data.items():
-        if value == '':
-            dataGood = False
+    answer = requests.post(SERVER_URL+'/searchUsers', data=data, headers={'Content-Type': 'application/json'})
+    print(answer.text)
+    return json.loads(answer.text)
 
-    if not dataGood:
-        messagebox.showerror("error", "data must be filled in")
-        return
+def getDataAboutOtherUser(username):
+    data={'username': username}
+    answer = requests.get(SERVER_URL+'/getPublicUserData', json=data, headers={'Content-Type': 'application/json'})
+    print(answer.text)
+    return json.loads(answer.text)
 
-    answer=requests.post(SERVER_URL+'/createNewUser2', json=datajson)
-    if answer.text == 'True':
-        USERNAME=entry_username.get()
-        PASSWORD=entry_password.get()
-        writeToFile(USERNAME,PASSWORD)
-        root.quit()
-        root.destroy()
-        welcome()
-    elif answer.text == 'False':
-        USERNAME=None
-        PASSWORD=None
-        messagebox.showerror("error", "user registration failed")
-
-def register_user():
-    root = tk.Tk()
-    root.title("client reg")
-
-    tk.Label(root, text="Username:").grid(row=0, column=0, padx=10, pady=5)
-    entry_username = tk.Entry(root)
-    entry_username.grid(row=0, column=1, padx=10, pady=5)
-
-    tk.Label(root, text="Password:").grid(row=2, column=0, padx=10, pady=5)
-    entry_password = tk.Entry(root, show="*")
-    entry_password.grid(row=2, column=1, padx=10, pady=5)
-
-    tk.Label(root, text="Momnum:").grid(row=3, column=0, padx=10, pady=5)
-    entry_momnum = tk.Entry(root)
-    entry_momnum.grid(row=3, column=1, padx=10, pady=5)
-
-    tk.Label(root, text="Razmer:").grid(row=4, column=0, padx=10, pady=5)
-    entry_razmer = tk.Entry(root)
-    entry_razmer.grid(row=4, column=1, padx=10, pady=5)
-
-    tk.Label(root, text="Pol:").grid(row=5, column=0, padx=10, pady=5)
-    entry_pol = tk.Entry(root)
-    entry_pol.grid(row=5, column=1, padx=10, pady=5)
-
-    tk.Button(root, text="Send", command=lambda: requestUserRegistration(entry_username, entry_password, entry_momnum, entry_razmer, entry_pol, root)).grid(row=6, column=0, columnspan=2, pady=10)
-
-    root.mainloop()
-
-def requestUserLogin(username, password):
-    global USERNAME, PASSWORD
-    data={
-        "username": username,
-        "password": password,
-    }
-    datajson = json.dumps(data)
-    print(datajson)
-
-
-    answer=requests.post(SERVER_URL+'/login', json=datajson)
-    if answer.text == 'True':
-        USERNAME=username
-        PASSWORD=password
-        writeToFile(USERNAME,PASSWORD)
-    elif answer.text == 'False':
-        USERNAME=None
-        PASSWORD=None
-        messagebox.showerror("error", "user login failed")
-
-def login_user():
-    root = tk.Tk()
-    root.title("client login")
-    tk.Label(root, text="Username:").grid(row=0, column=0, padx=10, pady=5)
-    entry_username = tk.Entry(root)
-    entry_username.grid(row=0, column=1, padx=10, pady=5)
-    tk.Label(root, text="Password:").grid(row=1, column=0, padx=10, pady=5)
-    entry_password = tk.Entry(root, show="*")
-    entry_password.grid(row=1, column=1, padx=10, pady=5)
-
-    tk.Button(root, text="Send",
-              command=lambda: requestUserLogin(entry_username.get(), entry_password.get())).grid(row=6, column=0, columnspan=2, pady=10)
-
-    root.mainloop()
-
+#tkinter windows
 def welcome():
-    root = tk.Tk()
-    root.title("welcome")
-    tk.Label(root, text="Welcome to FurryFans").grid(row=0, column=0, padx=10, pady=5)
+    frame1 = tk.Frame(root)
 
-    tk.Button(root, text="Login", command=login_user).grid(row=1, column=0, padx=10, pady=5)
+    label = tk.Label(frame1, text="Welcome to FurryFans")
+    label.grid(row=0, column=0)
 
-    tk.Button(root, text="Register", command=register_user).grid(row=2, column=0, padx=10, pady=5)
+    loginButton = tk.Button(frame1, text="Login", command=lambda: goto(login))
+    loginButton.grid(row=1, column=0)
 
-    root.mainloop()
+    registerButton = tk.Button(frame1, text="Register", command=lambda: goto(register))
+    registerButton.grid(row=2, column=0)
+
+    return frame1
+
+def login(*args):
+    frame=tk.Frame(root)
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(welcome))
+    backButton.grid(row=0, column=0)
+
+    login = tk.Label(frame, text="Login")
+    login.grid(row=0, column=1)
+
+    usernameLabel = tk.Label(frame, text="Username")
+    usernameLabel.grid(row=1, column=0)
+
+    usernameEntry = tk.Entry(frame, width=40)
+    usernameEntry.grid(row=1, column=1)
+
+    passwordLabel = tk.Label(frame, text="Password")
+    passwordLabel.grid(row=2, column=0)
+
+    passwordEntry = tk.Entry(frame, width=40)
+    passwordEntry.grid(row=2, column=1)
+
+    errorLabel = tk.Label(frame, text=args[0] if len(args) else "")
+    errorLabel.grid(row=3, column=0)
+
+    submitButton = tk.Button(frame, text="Login", command=lambda: checkLoginWrap(usernameEntry.get(), passwordEntry.get()))
+    submitButton.grid(row=4, column=0)
+    submitButton.grid(row=3, column=1)
+
+    return frame
+
+def register(*args):
+    frame = tk.Frame(root)
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(welcome))
+    backButton.grid(row=0, column=0)
+
+    registerLabel = tk.Label(frame, text="Register")
+    registerLabel.grid(row=0, column=1)
+
+    usernameLabel = tk.Label(frame, text="Username")
+    usernameLabel.grid(row=1, column=0)
+
+    usernameEntry = tk.Entry(frame, width=40)
+    usernameEntry.grid(row=1, column=1)
+
+    passwordLabel = tk.Label(frame, text="Password")
+    passwordLabel.grid(row=2, column=0)
+
+    passwordEntry = tk.Entry(frame, width=40)
+    passwordEntry.grid(row=2, column=1)
+
+    polLabel = tk.Label(frame, text="Pol")
+    polLabel.grid(row=3, column=0)
+
+    polEntry = tk.Entry(frame, width=40)
+    polEntry.grid(row=3, column=1)
+
+    nomerMamiLabel = tk.Label(frame, text="Nomer Mami")
+    nomerMamiLabel.grid(row=4, column=0)
+
+    nomerMamiEntry = tk.Entry(frame, width=40)
+    nomerMamiEntry.grid(row=4, column=1)
+
+    razmerLabel = tk.Label(frame, text="Razmer")
+    razmerLabel.grid(row=5, column=0)
+
+    razmerEntry = tk.Entry(frame, width=40)
+    razmerEntry.grid(row=5, column=1)
+
+    errorLabel = tk.Label(frame, text=args[0] if len(args) == 1 else "")
+    errorLabel.grid(row=6, column=0)
+
+    submitButton = tk.Button(frame, text="Register", command=lambda: createNewUserWrap(
+        usernameEntry.get(), passwordEntry.get(), polEntry.get(), nomerMamiEntry.get(), razmerEntry.get()))
+    submitButton.grid(row=6, column=1)
+
+    return frame
+
+def main():
+    if not checkLogin(USERNAME, PASSWORD):
+        #goto(login)
+        #return tk.Frame(root)
+        pass
+    mainFrame = tk.Frame(root)
+
+    furryFansLabel = tk.Label(mainFrame, text="FurryFans")
+    furryFansLabel.grid(row=0, column=0)
+
+    welcomeBackText = tk.Label(mainFrame, text="Welcome back, "+str(USERNAME))
+    welcomeBackText.grid(row=1, column=0)
+
+    createButton=tk.Button(mainFrame, text="Make post", command=lambda: goto(create))
+    createButton.grid(row=2, column=0)
+
+    subscribesButton=tk.Button(mainFrame, text="Subscribes", command=lambda: goto(subscribes))
+    subscribesButton.grid(row=3, column=0)
+
+    accountButton = tk.Button(mainFrame, text="account", command=lambda: goto(account))
+    accountButton.grid(row=4, column=0)
+
+    searchButton = tk.Button(mainFrame, text="Search", command=lambda: goto(search))
+    searchButton.grid(row=5, column=0)
 
 
-onStartup()
+    return mainFrame
 
-welcome()
+def create(*args):
+    frame = tk.Frame(root)
 
-#register_user()
-#login_user()
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(main))
+    backButton.grid(row=0, column=0)
+
+    filePathLabel = tk.Label(frame, text="File path: "+args[0] if len(args) >0 else "")
+    filePathLabel.grid(row=0, column=2)
+
+    titleLabel = tk.Label(frame, text="Title: ")
+    titleLabel.grid(row=1, column=0)
+
+    titleInput = tk.Entry(frame, width=40)
+    titleInput.grid(row=1, column=1)
+
+
+    selectFileButton = tk.Button(frame, text="upload file", command=lambda: selectFileWrap())
+    selectFileButton.grid(row=0, column=1)
+
+    errorLabel = tk.Label(frame, text=args[1] if len(args) >1 else "")
+    errorLabel.grid(row=2, column=0)
+
+    uploadButton = tk.Button(frame, text="Make post", command=lambda: makePost(args[0] if len(args) >0 else None, titleInput.get()))
+    uploadButton.grid(row=2, column=1)
+
+
+
+    return frame
+
+def account():
+    frame = tk.Frame(root)
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(main))
+    backButton.grid(row=0, column=0)
+
+    name = tk.Label(frame, text="Name: "+USERNAME)
+    name.grid(row=1, column=0)
+
+    logoutButton = tk.Button(frame, text="Logout", command=lambda: logout())
+    logoutButton.grid(row=1, column=1)
+
+    return frame
+
+def subscribes():
+    frame = tk.Frame(root)
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(main))
+    backButton.grid(row=0, column=0)
+
+    subs=getSubscribes()
+    for index, sub in enumerate(subs):
+        a=tk.Button(frame, command=lambda: goto(viewSomeone(sub)))
+        a.grid(row=index, column=1)
+    if len(subs)==0:
+        lbl=tk.Label(frame, text="No subscribes")
+        lbl.grid(row=0, column=1)
+
+
+    return frame
+
+def search(*args):
+    frame = tk.Frame(root)
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(main))
+    backButton.grid(row=0, column=0)
+
+    inputString = tk.Entry(frame, width=40)
+    inputString.grid(row=0, column=1)
+
+    inputString.insert(0, args[1] if len(args) >1 else "")
+
+    searchButton = tk.Button(frame, text="Search", command=lambda: goto(search, [requestSearch(inputString.get()), inputString.get()]))
+    searchButton.grid(row=0, column=2)
+
+    #print(args[0] if len(args) >0 else None)
+    if len(args)==0:
+        searchResults=[]
+    elif len(args)==1:
+        searchResults=args[0]
+
+    for i, result in enumerate(args[0] if len(args) >0 else []):
+        tk.Button(
+            frame,
+            text=str(result[1]),
+            command=lambda res=result: goto(viewSomeone, [res[1], args[0], inputString.get()])
+        ).grid(row=i + 1, column=1)
+        #print(result)
+
+    return frame
+
+def viewSomeone(*args):
+    if len(args) == 0:
+        goto(main)
+    #print('args', args)
+
+    frame = tk.Frame(root)
+    data=getDataAboutOtherUser(args[0])
+
+    backButton = tk.Button(frame, text="Back", command=lambda: goto(search, [args[1], args[2],]))
+    backButton.grid(row=0, column=0)
+
+    usernameLabel = tk.Label(frame, text="Username: "+args[0])
+    usernameLabel.grid(row=0, column=1)
+
+    polLabel = tk.Label(frame, text="Pol: "+data[0][0])
+    polLabel.grid(row=1, column=1)
+
+    nomerMamiLabel = tk.Label(frame, text="NomerMami: "+data[0][1])
+    nomerMamiLabel.grid(row=2, column=1)
+
+    razmerLabel = tk.Label(frame, text="Razmer: "+data[0][2])
+    razmerLabel.grid(row=3, column=1)
+
+
+
+    return frame
+
+
+onStart()
+if checkLogin(USERNAME, PASSWORD)==True:
+    main().pack()#expand = True)
+else:
+    welcome().pack()#expand = True)
+
+#welcome().pack()
+#main().pack(anchor=tk.N)
+root.geometry("800x600")
+root.mainloop()
--- server.py
+++ server.py
@@ -1,19 +1,27 @@
+import base64
+import rapidfuzz
 import flask
+from flask import request
 import sqlite3
 import hashlib
+import json
+from flask_apscheduler import APScheduler
 
 app = flask.Flask(__name__)
+
+
 
 with sqlite3.connect('database.db') as db:
     cursor = db.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
+    username TEXT UNIQUE,
     password TEXT,
     pol TEXT,
     NOMERMAMI TEXT,
-    RAZMER TEXT
+    RAZMER TEXT,
+    data TEXT
     )
     ''')
     db.commit()
@@ -37,39 +45,69 @@ def checkLogin(username, password):
             return True
         return False
 
-@app.route('/createNewUser/<username>/<password>/<pol>/<nomer_mami>/<razmer>')
-def createNewUser(username, password, pol, nomer_mami, razmer):
+def updateUserList():
+    global userList
+    #print("Updating user list")
     with sqlite3.connect('database.db') as db:
         cursor = db.cursor()
-        cursor.execute(f'''
-        SELECT * FROM users WHERE username = "{username}"''')
-        if cursor.fetchall() != []:
-            return 'False'
-
-    with sqlite3.connect('database.db') as db:
-        cursor = db.cursor()
-        cursor.execute(f'''
-        INSERT INTO users ('username', 'password', 'pol', 'NOMERMAMI', 'RAZMER') VALUES("{username}", "{hashlib.sha1(password.encode('utf-8')).hexdigest()}", "{pol}"," {nomer_mami}", "{razmer}")
+        cursor.execute('''
+        select id, username from users
         ''')
-        db.commit()
-    return 'True'
+        userList = cursor.fetchall()
+        #print(userList)
 
-@app.route('/newPost/<username>/<password>/<data>')
-def newPost(username, password, data):
-    if not checkLogin(username, password):
-        return 'False'
+@app.route('/createNewUser2', methods=['POST'])
+def createNewUser2():
+    data = request.get_json(force=True)
+    #data=json.loads(json_data)
+    print(data)
+    print(data['username'])
+
+    for key, value in data.items():
+        print(key, value)
+        if value == '':
+            return 'All data must be filled in'
 
     with sqlite3.connect('database.db') as db:
         cursor = db.cursor()
-        cursor.execute(f'''INSERT INTO posts ('username', 'data') VALUES("{username}", "{data}")''')
+        cursor.execute(f'''
+         SELECT * FROM users WHERE username = "{data['username'].lower()}"''')
+        if cursor.fetchall() != []:
+            return 'Username already exists'
+
+    userData = base64.b64encode(json.dumps({'subscribes':[], }).encode('utf-8')).decode('utf-8')
+
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        cursor.execute(f'''
+         INSERT INTO users ('username', 'password', 'pol', 'NOMERMAMI', 'RAZMER', 'data') VALUES("{data['username']}", "{hashlib.sha1(data['password'].encode('utf-8')).hexdigest()}", "{data['pol']}"," {data['momnum']}", "{data['razmer']}", "{userData}")
+         ''')
+        db.commit()
+    return 'True'
+
+@app.route('/newPost/', methods=['POST'])
+def newPost():
+    jsonData = request.get_json(force=True)
+    if not checkLogin(jsonData['username'], jsonData['password']):
+        return 'Wrong credentials'
+
+    data = base64.b64encode(json.dumps({'title': jsonData['title'], 'fileTitle':jsonData['fileTitle'], 'file': jsonData['file']}).encode('utf-8')).decode('utf-8')
+
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        print(f'''INSERT INTO posts ('username', 'data') VALUES("{jsonData['username']}", "{data}")''')
+        cursor.execute(f'''INSERT INTO posts ('username', 'data') VALUES("{jsonData['username']}", "{data}")''')
         db.commit()
 
     return 'True'
 
-@app.route('/login/<username>/<password>')
-def login(username, password):
+@app.route('/login/', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
     if not checkLogin(username, password):
-        return 'False'
+        return 'Username or password\n is incorrect'
     return 'True'
 
 @app.route('/getPosts/<username>')
@@ -79,5 +117,52 @@ def getPosts(username):
         cursor.execute(f'''SELECT * FROM posts WHERE username = "{username}"''')
         return cursor.fetchall()
 
+@app.route('/getSubscribes')
+def getSubscribes():
+    data=request.get_json(force=True)
 
-app.run(host='0.0.0.0', port=8080)
+    if not checkLogin(data['username'], data['password']):
+        return 'invalid credentials'
+
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        cursor.execute(f'''SELECT data FROM users WHERE username = "{data['username']}"''')
+        userdata = json.loads(base64.b64decode(cursor.fetchall()[0][0]).decode('utf-8'))
+    print(userdata)
+    return userdata['subscribes']
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    data = request.get_json(force=True)
+    if not checkLogin(data['username'], data['password']):
+        return 'invalid credentials'
+
+    with sqlite3.connect('database.db') as db:
+        pass
+
+@app.route('/searchUsers/', methods=['POST'])
+def searchUsers():
+    global userList
+    search = request.get_json(force=True)['search']
+    bestResults = sorted(userList, key=lambda tupl: rapidfuzz.fuzz.ratio(search, tupl[1]), reverse=True)[:10]
+
+    return json.dumps(bestResults)
+
+@app.route('/getPublicUserData/', methods=['GET'])
+def getPublicUserData():
+    data=request.get_json(force=True)
+    username = data['username']
+
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        cursor.execute(f'''SELECT pol, NOMERMAMI, RAZMER FROM users WHERE username = "{username}"''')
+        userdata = cursor.fetchall()
+    return json.dumps(userdata)
+
+
+updateUserList()
+
+scheduler = APScheduler()
+scheduler.add_job(func=updateUserList, trigger='interval', seconds=5, id='updateUserList')
+scheduler.start()
+app.run()#host='0.0.0.0', port=8080, )#debug=True)
